3ae43ceb3e5a78f258b280d9977427abc1bc093f
jiarui <422463270@qq.com>
2020-08-25 12:45:10 +0800

    添加了学术讲座Lectures的相关实体对象与操作； 按照新增的example写法重新实现了功能，包括分页检索和根据包含关键字搜索，此外还有根据输入字段排序

---
+++ dldsj-server/src/main/java/edu/zju/gis/dldsj/server/entity/Lecture.java
@@ -0,0 +1,25 @@
+package edu.zju.gis.dldsj.server.entity;
+
+import edu.zju.gis.dldsj.server.base.BaseFilter;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.Date;
+
+/**
+ * @author Jiarui
+ * @date 2020/8/19
+ */
+
+@Getter
+@Setter
+@ToString(callSuper = true)
+public class Lecture extends BaseFilter<String> {
+    String name;
+    String speaker;
+    String time;
+    String place;
+    String href;
+}
+
