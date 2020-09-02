1bbf11d846e3cd3f1031c10ee2627da21b0abb40
jiarui <422463270@qq.com>
2020-09-02 13:14:37 +0800

    修改文献检索功能，删除了之前单独的根据目录和时间检索功能，并把它们统一整合到了search中，使得用户可以在搜索结果之后进行第二轮筛选；修改了唯一值计数功能，使之可以根据用户搜索结果动态更改。

--- dldsj-server/src/main/java/edu/zju/gis/dldsj/server/service/LiteratureService.java
+++ dldsj-server/src/main/java/edu/zju/gis/dldsj/server/service/LiteratureService.java
@@ -1,10 +1,8 @@
 package edu.zju.gis.dldsj.server.service;
 
 import edu.zju.gis.dldsj.server.base.BaseService;
-import edu.zju.gis.dldsj.server.common.Page;
 import edu.zju.gis.dldsj.server.entity.Literature;
-
-import java.util.Date;
+import edu.zju.gis.dldsj.server.entity.searchPojo.LiteratureSearchPojo;
 import java.util.Map;
 
 /**
@@ -13,10 +11,6 @@ import java.util.Map;
  */
 public interface LiteratureService extends BaseService<Literature,String> {
 
-    Page<Literature> selectBySource(String source,Page page);
-
     //根据输入字段名称，返回结果的唯一不同值与对应数量
-    Map<String,String> getDistinctField(String field);
-
-    Page<Literature> selectByTime(String time1,String time2,Page page);
+    Map<String,String> getDistinctField(LiteratureSearchPojo param);
 }
--- dldsj-server/src/main/java/edu/zju/gis/dldsj/server/service/impl/LiteratureServiceImpl.java
+++ dldsj-server/src/main/java/edu/zju/gis/dldsj/server/service/impl/LiteratureServiceImpl.java
@@ -1,15 +1,12 @@
 package edu.zju.gis.dldsj.server.service.impl;
 
-import com.github.pagehelper.PageHelper;
 import edu.zju.gis.dldsj.server.base.BaseServiceImpl;
-import edu.zju.gis.dldsj.server.common.Page;
-import edu.zju.gis.dldsj.server.entity.Geodata;
 import edu.zju.gis.dldsj.server.entity.Literature;
+import edu.zju.gis.dldsj.server.entity.searchPojo.LiteratureSearchPojo;
 import edu.zju.gis.dldsj.server.mapper.LiteratureMapper;
 import edu.zju.gis.dldsj.server.service.LiteratureService;
 import org.springframework.stereotype.Service;
 
-import javax.swing.*;
 import java.util.*;
 
 /**
@@ -19,42 +16,56 @@ import java.util.*;
 @Service
 public class LiteratureServiceImpl extends BaseServiceImpl<LiteratureMapper, Literature,String> implements LiteratureService {
 
-    //数据目录
-    public Page<Literature> selectBySource (String source,Page page){
-        PageHelper.startPage(page.getPageNo(),page.getPageSize());
-        return new Page<>(mapper.selectBySource(source));
-    }
-
     //根据输入字段名称，返回结果的唯一不同值与对应数量
-    public Map<String,String> getDistinctField(String field) {
-        //field为查找的字段名，如TYPE、UPLOADER等
-        List<Literature> list = mapper.getDistinctField(field);
+    public Map<String,String> getDistinctField(LiteratureSearchPojo param) {
+        //field为查找的字段名，SOURCE、YEAR、AU_AFFILIATION
+        String field = param.getDistinctField();
+        System.out.println(field);
+        List<Literature> list = new ArrayList<>();
+        switch (field){
+            case "SOURCE":
+                list = mapper.getDistinctField(param);
+                break;
+            case "YEAR":
+                list = mapper.getDistinctField(param);
+                break;
+            case "AU_AFFILIATION":
+                list = mapper.getDistinctField(param);
+                break;
+            default:
+                list = null;
+        }
+        System.out.println(list);
         List<String> res = new ArrayList<String>();
         //str为读取出来的唯一字段名称
         String str;
-        //用来存储最后结果，样式如TIF 2
+        //用来存储最后结果，样式如2020 5
         Map<String,String> map = new HashMap<String, String>();
 
         for (Literature literature : list){
+            String count;
             switch (field) {
                 case "SOURCE":
                     str = literature.getSource();
+                    param.setSourceDistinct(str);
+                    count = mapper.getCountOfField(param);
+                    map.put(str,count);
                     break;
+                case "YEAR":
+                    str = literature.getYear();
+                    param.setYearDistinct(str);
+                    count = mapper.getCountOfField(param);
+                    map.put(str,count);
+                    break;
+                case "AU_AFFILIATION":
+                    str = literature.getAuthorAffiliation();
+                    param.setAuthorAffiliationFilterDistinct(str);
+                    count = mapper.getCountOfField(param);
+                    map.put(str,count);
                 default:
                     str = "xxx";
             }
-            String count;
-            count = mapper.getCountOfField(field,str);
-            map.put(str,count);
         }
         return map;
-    }
-
-    //根据时间查询
-    public Page<Literature> selectByTime(String time1,String time2,Page page){
-        Integer start = Integer.parseInt(time1);
-        Integer end = Integer.parseInt(time2);
-        PageHelper.startPage(page.getPageNo(),page.getPageSize());
-        return new Page<>(mapper.selectByTime(start,end,page));
     }
 }
