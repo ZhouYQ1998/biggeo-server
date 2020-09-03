04a457df6da796e0472f008f3d19b44d3fc431d3
jiarui <422463270@qq.com>
2020-09-03 18:31:05 +0800

    删去了多余的引用

--- dldsj-server/src/main/java/edu/zju/gis/dldsj/server/service/GeodataService.java
+++ dldsj-server/src/main/java/edu/zju/gis/dldsj/server/service/GeodataService.java
@@ -3,8 +3,6 @@ package edu.zju.gis.dldsj.server.service;
 import edu.zju.gis.dldsj.server.base.BaseService;
 import edu.zju.gis.dldsj.server.common.Page;
 import edu.zju.gis.dldsj.server.entity.Geodata;
-import javax.servlet.http.HttpServletResponse;
-import java.io.FileNotFoundException;
 import java.util.Map;
 
 /**
@@ -16,10 +14,8 @@ public interface GeodataService extends BaseService<Geodata,String> {
     //数据目录功能，根据type、uploader或source等目录字段，返回数据(单选)
     Page<Geodata> selectByType1(String type,Page page);
     Page<Geodata> selectByType2(String type,Page page);
-    Page<Geodata> selectByUploader(String uploader,Page page);
     Page<Geodata> selectBySource(String source,Page page);
 
     //根据输入字段名称，返回结果的唯一不同值与对应数量
     Map<String,String> getDistinctField(String field);
-
     }
