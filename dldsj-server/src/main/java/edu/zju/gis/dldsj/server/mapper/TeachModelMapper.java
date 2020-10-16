37e5de3b966cd7e4231fbe659581debe29167eb6
jiarui <422463270@qq.com>
2020-10-16 10:48:50 +0800

    增加了TeachModelMapper

---
+++ dldsj-server/src/main/java/edu/zju/gis/dldsj/server/mapper/TeachModelMapper.java
@@ -0,0 +1,12 @@
+package edu.zju.gis.dldsj.server.mapper;
+
+import edu.zju.gis.dldsj.server.base.BaseMapper;
+import edu.zju.gis.dldsj.server.entity.TeachModel;
+
+/**
+ * @author Jiarui
+ * @date 2020/10/12
+ */
+public interface TeachModelMapper extends BaseMapper<TeachModel,String> {
+
+}
