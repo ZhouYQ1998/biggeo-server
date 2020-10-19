d874a620e0906fcfdd471cd47c07697c41a51375
Wang Luoqi <1446735020@qq.com>
2020-10-19 14:19:09 +0800

    Update code runner.

--- dldsj-server/src/main/java/edu/zju/gis/dldsj/server/controller/RunCodeController.java
+++ dldsj-server/src/main/java/edu/zju/gis/dldsj/server/controller/RunCodeController.java
@@ -1,51 +1,37 @@
-package edu.zju.gis.dldsj.server.config;
+package edu.zju.gis.dldsj.server.controller;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
+import edu.zju.gis.dldsj.server.dto.CodeModel;
+import edu.zju.gis.dldsj.server.dto.ProcessResult;
+import edu.zju.gis.dldsj.server.service.RunCodeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author : Shaojian
  * @date : 20201016
  */
 
-@ConfigurationProperties(prefix = "run.script")
-@Component
-public class RunCodeConfig {
-    private String cpp;
-    private String c;
-    private String python;
-    private String java;
+@Controller
+@CrossOrigin("*")
+public class RunCodeController {
 
-    public String getJava() {
-        return java;
+    @Autowired
+    public final RunCodeService runCode;
+
+    @Autowired
+    public RunCodeController(RunCodeService runCode) {
+        this.runCode = runCode;
     }
 
-    public void setJava(String java) {
-        this.java = java;
+    @RequestMapping(value = "/get",method = RequestMethod.GET)
+    public String xcxx() {
+        return "sdfsdfsdf";
     }
 
-    public void setCpp(String cpp) {
-        this.cpp = cpp;
+    @PostMapping("/run")
+    @ResponseBody
+    public ProcessResult runCode(@RequestBody CodeModel codeModel) throws Exception {
+        return runCode.runCode(codeModel.getType(), codeModel.getCode());
     }
-
-    public void setC(String c) {
-        this.c = c;
-    }
-
-    public void setPython(String python) {
-        this.python = python;
-    }
-
-    public String getCpp() {
-        return cpp;
-    }
-
-    public String getC() {
-        return c;
-    }
-
-    public String getPython() {
-        return python;
-    }
-
 }
